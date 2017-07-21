b233440f0d5d653dae704cc36418d0d5457fa3fc
Aaron Jones <aaronpjones00@gmail.com>
2017-07-21 12:52:01 +0100

    added castle windsor dependency injection to project

--- FizzBuzz/FizzBuzz/Global.asax.cs
+++ FizzBuzz/FizzBuzz/Global.asax.cs
@@ -1,14 +1,38 @@
 ﻿using System.Web;
 using System.Web.Http;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
 using FizzBuzz.App_Start;
+using FizzBuzz.Windsor;
 
 namespace FizzBuzz
 {
     public class WebApiApplication : HttpApplication
     {
+        private readonly WindsorContainer _container;
+
+        public WebApiApplication()
+        {
+            _container = new WindsorContainer();
+        }
+
         protected void Application_Start()
         {
+            InstallDependencies();
+            RegisterDependencyResolver();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+
+        private void InstallDependencies()
+        {
+            _container.Install(FromAssembly.This());
+        }
+
+        private void RegisterDependencyResolver()
+        {
+            GlobalConfiguration.Configuration.DependencyResolver = 
+                new WindsorDependencyResolver(_container.Kernel);
         }
     }
 }
