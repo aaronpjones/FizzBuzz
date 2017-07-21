d35326fdc850482365511db45fdbd07286ee3384
Aaron Jones <aaronpjones00@gmail.com>
2017-07-21 09:33:03 +0100

    created mock api + stubbed test

---
+++ FizzBuzz/FizzBuzz.Test.Unit/Api/FizzBuzzControllerTests/When_calculating_FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using FizzBuzz.Api;
+using NUnit.Framework;
+
+namespace FizzBuzz.Test.Unit.Api.FizzBuzzControllerTests
+{
+    [TestFixture]
+    public class When_calculating_FizzBuzz
+    {
+        private FizzBuzzController _fizzBuzzController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fizzBuzzController = new FizzBuzzController();
+        }
+
+        [Test]
+        public void Ok_content_result_returned()
+        {
+            IHttpActionResult result = _fizzBuzzController.CalculateFizzBuzz(new[] {1, 20});
+            Assert.That(((dynamic)result).Content, Is.Not.Null);
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Api/FizzBuzzController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace FizzBuzz.Api
+{
+    public class FizzBuzzController : ApiController
+    {
+        [HttpPost, Route("api/fizzbuzz/")]
+        public IHttpActionResult CalculateFizzBuzz([FromBody] int[] integerRange)
+        {
+            return Ok(new {
+                Result = "1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz",
+                Summary = new
+                {
+                    Fizz = 5,
+                    Buzz = 3,
+                    FizzBuzz = 1,
+                    Integer = 11
+                }
+            });
+        }
+    }
+}
